02fa0f20cf925ebd259cf658f115ffcc36d5c9f4
ARJUN GAWANDE <156084218+ArjunGawande@users.noreply.github.com>
2024-07-14 21:36:42 +0530

    Add files via upload

---
+++ zod.js
@@ -0,0 +1,10 @@
+const zod = require('zod');
+const emailSchema = zod.string().email();
+const passwordSchema = zod.string().min(6);
+
+// Define signupSchema using CommonJS syntax
+module.exports.signupSchema = zod.object({
+  email: emailSchema,
+  password: passwordSchema,
+  // name: nameSchema, // Uncomment and add nameSchema if needed
+});
